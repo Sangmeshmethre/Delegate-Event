6e39aee3e5cfec967b09dc87e4df0cc85aa21ba3
Sangmesh Methre <SangmeshMetre>
2022-11-12 21:20:01 +0530

    Event Demo

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDemo
+{
+    public delegate void DelEventHandler();
+    class program
+    {
+        public static event DelEventHandler add;
+        public static void Main(string[] args)
+        {
+            add += new DelEventHandler(India);
+            add += new DelEventHandler(USA);
+            add += new DelEventHandler(England);
+
+            add.Invoke();
+            Console.WriteLine("************************Event Handling****************");
+            EventHandling.ImplementEvent();
+        }
+        private static void India()
+        {
+            Console.WriteLine("India");
+        }
+        private static void USA()
+        {
+            Console.WriteLine("USA");
+        }
+        private static void England()
+        {
+            Console.WriteLine("Englend");
+        }
+    }
+}
